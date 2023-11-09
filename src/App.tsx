306b9be789bac2cd361eca9b77ad069ac5ee44ea
Alexander Shchirov <shchirov1998@mail.ru>
2023-11-09 12:08:41 +0300

    commit-3(+main)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import {Header} from "./layaut/header/Header";
+import {Main} from "./layaut/sections/main/Main";
 
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <Main/>
         </div>
     );
 }
--- src/layaut/sections/main/Main.tsx
+++ src/layaut/sections/main/Main.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
+import styled from "styled-components";
+import photo from '../../../assets/images/foto.webp'
+import {FlexWrapper} from "../../../components/FlexWrapper";
 
 export const Main = () => {
     return (
-        <div>
-            <div>
-                <span>Hi There</span>
-                <span>I am Alexander Shchirov</span>
-            </div>
-        </div>
+        <StyledMain>
+            <FlexWrapper align={"center"} justify={"space-around"}>
+                <div>
+                    <span>Hi There</span>
+                    <Name>I am Alexander Shchirov</Name>
+                    <MainTitile>A Web Developer.</MainTitile>
+                </div>
+                <Photo src={photo} alt="my foto"/>
+            </FlexWrapper>
+        </StyledMain>
     );
 };
+
+const StyledMain = styled.div`
+  min-height: 100vh;
+  background-color: #fff4d0;
+`
+
+
+
+const Photo = styled.img`
+  width: 350px;
+  height: 430px;
+  object-fit: cover;
+
+`
+const MainTitile = styled.h1`
+
+`
+
+const Name= styled.h2`
+
+`
